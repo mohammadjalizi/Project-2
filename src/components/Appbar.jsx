f44af3eef065154ef42401f8936e33b0621acad3
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-29 16:19:09 +0330

    added project

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -57,9 +57,80 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
       },
     },
   }));
-
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+  }));
 
 const Appbarr = () => {
+    const menumobile=useRef(null)
+    const renderMobileMenu = (
+        <Menu
+          anchorEl={menumobile.current}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        //   id={mobileMenuId}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        //   open={isMobileMenuOpen}
+        //   onClose={handleMobileMenuClose}
+        >
+          <MenuItem>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="error">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <p>Messages</p>
+          </MenuItem>
+          <MenuItem>
+            <IconButton
+            ref={menumobile}
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <p>Notifications</p>
+          </MenuItem>
+          <MenuItem 
+          
+          
+        //   onClick={fa}
+          
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="primary-search-account-menu"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <p>Profile</p>
+          </MenuItem>
+        </Menu>
+      );
+    
   return (
     <div>
    <AppBar position="static">
@@ -132,6 +203,7 @@ const Appbarr = () => {
           </Box>
         </Toolbar>
       </AppBar>
+      {renderMobileMenu}
     </div>
   )
 }
