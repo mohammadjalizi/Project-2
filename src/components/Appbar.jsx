eb231850a9320da827f6a9c3102abd770fa326a3
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-29 16:50:52 +0330

    added project

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -79,68 +79,16 @@ SetshowmobileBiggscreen(false)
 
           }}
         >
-          <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-          <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+          <MenuItem onClick={() => {
+            SetshowmobileBiggscreen(false)
+          }
+          }>Profile</MenuItem>
+          <MenuItem onClick={() => {
+            SetshowmobileBiggscreen(false)
+          }
+          }>My account</MenuItem>
         </Menu>
-  const renderMobileMenu = (
-        <Menu
-          anchorEl={menumobile.current}
-       
-        //   id={mobileMenuId}
-        
-       open={shownobile}
-        //   open={isMobileMenuOpen}
-          onClose={()=>{
-Setshowmobile(false)
-
-          }}
-        >
-          <MenuItem   
-    
-          >
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <p>Messages</p>
-          </MenuItem>
-          <MenuItem>
-            <IconButton
-         
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <p>Notifications</p>
-          </MenuItem>
-          <MenuItem 
-          
-          
-          onClick={()=>{
-            Setshowmobile(false)
-            
-                      }}
-          
-          >
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="primary-search-account-menu"
-              aria-haspopup="true"
-              color="inherit"
-        
-            >
-              <AccountCircle />
-            </IconButton>
-            <p>Profile</p>
-          </MenuItem>
-        </Menu>
-      );
+    );
     
   return (
     <div>
