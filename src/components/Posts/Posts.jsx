c06d02dae644d7497dede46f041cc92bd3add18c
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-31 13:32:03 +0330

    added project

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -75,7 +75,7 @@ const Posts = () => {
     <Box sx={{ flexGrow: "3" }}>
       {myCards.map((item) => {
         return (
-          <Card sx={{ maxWidth: 444, mx: "auto", my: 5 }}>
+          <Card  key={item.imgLink}  sx={{ maxWidth: 444, mx: "auto", my: 5 }}>
             <CardHeader
               avatar={
                 <Avatar sx={{ color:theme.palette.getContrastText(item.color)  ,bgcolor: item.color }} aria-label="recipe">
