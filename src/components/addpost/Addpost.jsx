1114839a6f3a6ea8bba2d4302a2b96ac0d0360a0
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-31 12:58:35 +0330

    added project

--- src/components/addpost/Addpost.jsx
+++ src/components/addpost/Addpost.jsx
@@ -5,11 +5,29 @@ import React from 'react'
 
 const Addpost = () => {
   return (
+    <>       
     <Tooltip title="Add Post" sx={{position:"fixed",left:"22px",bottom:"22px"}}>   
     <Fab color="primary" aria-label="add">
     <AddIcon />
   </Fab>
   </Tooltip>
+  <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+
+  </>
   )
 }
 
