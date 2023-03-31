c1c553ae3e58dc766cdbf1ea5a60ad203c6368df
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-31 11:16:43 +0330

    added project

--- src/components/rightBar/Rightbar.jsx
+++ src/components/rightBar/Rightbar.jsx
@@ -1,6 +1,7 @@
 import { Box, Typography } from '@mui/material'
 import Avatar from '@mui/material/Avatar';
 import AvatarGroup from '@mui/material/AvatarGroup';
+
 import React from 'react'
 
 const Rightbar = () => {
@@ -10,10 +11,10 @@ const Rightbar = () => {
     Online Frinds
 </Typography>
 <AvatarGroup total={24}>
-  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-  <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-  <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-  <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+  <Avatar alt="Remy Sharp" src="https://mui.com//static/images/avatar/1.jpg" />
+  <Avatar alt="Travis Howard" src="https://mui.com//static/images/avatar/2.jpg" />
+  <Avatar alt="Agnes Walker" src="https://mui.com//static/images/avatar/4.jpg" />
+  <Avatar alt="Trevor Henderson" src="https://mui.com//static/images/avatar/5.jpg" />
 </AvatarGroup>
 <Typography variant="body1" >
    Latest Photo
