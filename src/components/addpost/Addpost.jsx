d8576bfb735ef40ea549952b6422200ce664dc88
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-31 12:51:25 +0330

    added project

--- src/components/addpost/Addpost.jsx
+++ src/components/addpost/Addpost.jsx
@@ -1,8 +1,13 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
 const Addpost = () => {
   return (
-    <div>Addpost</div>
+    <Box>   
+    <Fab color="primary" aria-label="add">
+    <AddIcon />
+  </Fab>
+  </Box>
   )
 }
 
