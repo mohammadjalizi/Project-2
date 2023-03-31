4708ca5253c59ff6814f8a14ec6d589466f37c17
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-31 12:56:35 +0330

    added project

--- src/components/addpost/Addpost.jsx
+++ src/components/addpost/Addpost.jsx
@@ -1,11 +1,11 @@
-import { Box, Fab, Tooltip } from '@mui/material'
+import {  Fab, Tooltip } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
 
 import React from 'react'
 
 const Addpost = () => {
   return (
-    <Tooltip title="Add Post">   
+    <Tooltip title="Add Post" sx={{position:"fixed",left:"22px",bottom:"22px"}}>   
     <Fab color="primary" aria-label="add">
     <AddIcon />
   </Fab>
