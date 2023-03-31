67a766eff14612b36601cdeb515774a519168587
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-31 11:03:12 +0330

    added project

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,150 +1,129 @@
-import React, { useRef, useState } from 'react'
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Box, Menu, MenuItem } from '@mui/material';
+import React, { useRef, useState } from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Box, Checkbox, Menu, MenuItem } from "@mui/material";
+import {
+  Bookmark,
+  BookmarkBorder,
+  Favorite,
+  FavoriteBorder,
+} from "@mui/icons-material";
+
 const Posts = () => {
-    const myCards = [
-        {
-          letter: "K",
-          color: "royalblue",
-          userName: "mohammadjalizi",
-          imgLink:
-            "https://images.pexels.com/photos/3480792/pexels-photo-3480792.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        },
-        {
-          letter: "A",
-          color: "purple",
-          userName: "Ali Hassan",
-          imgLink:
-            "https://images.pexels.com/photos/1549280/pexels-photo-1549280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        },
-        {
-          letter: "N",
-          color: "teal",
-          userName: "Alaa Hassan",
-          imgLink:
-            "https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&w=600",
-        },
-        {
-          letter: "M",
-          color: "royalblue",
-          userName: "Walaa Hassan",
-          imgLink:
-            "https://images.pexels.com/photos/307008/pexels-photo-307008.jpeg?auto=compress&cs=tinysrgb&w=600",
-        },
-      ];    
-      const [anchorEl, setAnchorEl] = useState(null);
-      const [cardmenu, setcardmenu] = useState(false);
-    
-      const refMenuBiggerScreen = useRef(null);
-      const [showBiggerScreenMenu, setscardmenu] = useState(false);
-    
+  const myCards = [
+    {
+      letter: "K",
+      color: "royalblue",
+      userName: "mohammadjalizi",
+      imgLink:
+        "https://images.pexels.com/photos/3480792/pexels-photo-3480792.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      letter: "A",
+      color: "purple",
+      userName: "do3aa Hassan",
+      imgLink:
+        "https://images.pexels.com/photos/1549280/pexels-photo-1549280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      letter: "N",
+      color: "teal",
+      userName: "Alaa Hassan",
+      imgLink:
+        "https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      letter: "M",
+      color: "royalblue",
+      userName: "Walaa Hassan",
+      imgLink:
+        "https://images.pexels.com/photos/307008/pexels-photo-307008.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+  ];
 
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-      const renderMenu = (
-        <Menu
-          anchorEl={refMenuBiggerScreen.current}
-          open={showBiggerScreenMenu}
-          onClose={() => {
-            setcardmenu(false);
-          }}
-        >
-          <MenuItem
-            onClick={() => {
-              setcardmenu(false);
-            }}
-          >
-            Profile
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-                (false);
-            }}
-          >
-            My account
-          </MenuItem>
-        </Menu>
-      );
-      const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-setcardmenu(true)
-      };
-      const handleClose = () => {
-        setAnchorEl(null);
+  const renderMenu = (
+    <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+      <MenuItem onClick={handleClose}>Profile</MenuItem>
+      <MenuItem onClick={handleClose}>My account</MenuItem>
+    </Menu>
+  );
 
-      };
-    
-    
-    
   return (
-    <Box sx={{flexGrow:"3"}}>
+    <Box sx={{ flexGrow: "3" }}>
+      {myCards.map((item) => {
+        return (
+          <Card sx={{ maxWidth: 444, mx: "auto", my: 5 }}>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: item.color }} aria-label="recipe">
+                  {item.letter}
+                </Avatar>
+              }
+              action={
+                <IconButton onClick={handleClick} aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title={item.userName}
+              subheader="September 14, 2016"
+            />
+            <CardMedia
+              component="img"
+              height="194"
+              image={item.imgLink}
+              alt="Paella dish"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                This impressive paella is a perfect party dish and a fun meal to
+                cook together with your guests. Add 1 cup of frozen peas along
+                with the mussels, if you like.
+              </Typography>
+            </CardContent>
 
+            <CardActions disableSpacing>
+              <Checkbox
+                icon={<FavoriteBorder />}
+                checkedIcon={<Favorite sx={{ color: "red" }} />}
+              />
 
-   {myCards.map(item=>{
-return(
-    <Card sx={{ maxWidth: 444,mx:"auto" }}>
-    <CardHeader
-      avatar={
-        <Avatar sx={{ bgcolor: item.color }} aria-label="recipe">
-        {item.letter}
-        </Avatar>
-      }
-      action={
-        <IconButton onClick={(eo)=>{
-        handleClick()
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton>
 
-        }}   aria-label="settings">
-          <MoreVertIcon />
-        </IconButton>
-      }
-      title={item.userName}
-      subheader="September 14, 2016"
-    />
-    <CardMedia
-      component="img"
-      height="194"
-      image={item.imgLink}
-      alt="Paella dish"
-    />
-    <CardContent>
-      <Typography variant="body2" color="text.secondary">
-        This impressive paella is a perfect party dish and a fun meal to cook
-        together with your guests. Add 1 cup of frozen peas along with the mussels,
-        if you like.
-      </Typography>
-    </CardContent>
-    <CardActions disableSpacing>
-      <IconButton aria-label="add to favorites">
-      <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
-      </IconButton>
-      <IconButton aria-label="share">
-        <ShareIcon />
-      </IconButton>
+              <Box flexGrow={1} />
 
-    </CardActions>
-   
-  </Card>
+              <Checkbox icon={<BookmarkBorder />} checkedIcon={<Bookmark />} />
+            </CardActions>
+          </Card>
+        );
+      })}
 
-)
+      {renderMenu}
+    </Box>
+  );
+};
 
-   })}
-    {renderMenu}
-  </Box>
-
-  )
-}
-
-export default Posts
+export default Posts;
