90810a6114934379c98dd7731c0e4e5d601e8821
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-31 11:14:50 +0330

    added project

--- src/components/rightBar/Rightbar.jsx
+++ src/components/rightBar/Rightbar.jsx
@@ -1,12 +1,20 @@
 import { Box, Typography } from '@mui/material'
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
 import React from 'react'
 
 const Rightbar = () => {
   return (
-   <Box  flexGrow={1}>
+   <Box  flexGrow={2}>
 <Typography variant="body1" >
     Online Frinds
 </Typography>
+<AvatarGroup total={24}>
+  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+  <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+  <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+  <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+</AvatarGroup>
 <Typography variant="body1" >
    Latest Photo
 </Typography>
