883ddb7e5c377daa74d7a4afece3ae5355362719
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-31 11:10:53 +0330

    added project

--- src/components/rightBar/Rightbar.jsx
+++ src/components/rightBar/Rightbar.jsx
@@ -1,11 +1,21 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const Rightbar = () => {
   return (
    <Box>
+<Typography variant="body1" >
+    Online Frinds
+</Typography>
+<Typography variant="body1" >
+   Latest Photo
+</Typography>
 
-    
+<Typography variant="body1" >
+    Online Frinds
+</Typography>
+
+
    </Box>
   )
 }
