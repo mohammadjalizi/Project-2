3fe9b66fc48bf47f06d52e8b9db9001efebc8b7e
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-31 11:20:52 +0330

    added project

--- src/components/rightBar/Rightbar.jsx
+++ src/components/rightBar/Rightbar.jsx
@@ -10,12 +10,30 @@ const Rightbar = () => {
 <Typography variant="body1" >
     Online Frinds
 </Typography>
-<AvatarGroup total={24}>
-  <Avatar alt="Remy Sharp" src="https://mui.com//static/images/avatar/1.jpg" />
-  <Avatar alt="Travis Howard" src="https://mui.com//static/images/avatar/2.jpg" />
-  <Avatar alt="Agnes Walker" src="https://mui.com//static/images/avatar/4.jpg" />
-  <Avatar alt="Trevor Henderson" src="https://mui.com//static/images/avatar/5.jpg" />
-</AvatarGroup>
+<AvatarGroup sx={{ justifyContent: "center", my: 2 }} total={24}>
+        <Avatar
+          sx={{ width: 49, height: 45 }}
+          alt="Travis Howard"
+          src="https://images.pexels.com/photos/3662979/pexels-photo-3662979.jpeg"
+        />
+
+        <Avatar
+          sx={{ width: 49, height: 45 }}
+          alt="Remy Sharp"
+          src="https://images.pexels.com/photos/1516196/pexels-photo-1516196.jpeg"
+        />
+
+        <Avatar
+          sx={{ width: 49, height: 45 }}
+          alt="Agnes Walker"
+          src="https://images.pexels.com/photos/4029925/pexels-photo-4029925.jpeg"
+        />
+        <Avatar
+          sx={{ width: 49, height: 45 }}
+          alt="Trevor Henderson"
+          src="https://images.pexels.com/photos/5951914/pexels-photo-5951914.jpeg"
+        />
+      </AvatarGroup>
 <Typography variant="body1" >
    Latest Photo
 </Typography>
